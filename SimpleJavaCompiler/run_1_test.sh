2a5ba1729a03953b2d58f0eddb7cbb1f68eec140
Yaroslav <Yaroslaw.serow@yandex.ru>
2020-05-25 10:10:51 +0300

    Update run_1_test.sh

--- SimpleJavaCompiler/run_1_test.sh
+++ SimpleJavaCompiler/run_1_test.sh
@@ -6,9 +6,9 @@ pwd
 make
 rm -rf ../answers
 mkdir ../answers
-touch ../answers/$2
-touch ../answers/$3
 
-./Simplejavacompiler ../$1 ../answers/$2
-dot -Tsvg ../answers/$2 -o ../answers/$3
-rm ../answers/$2
+touch ../answers/graphiz_output.dot
+touch ../answers/out.svg
+./Simplejavacompiler ../$1 ../answers/graphiz_output.dot
+dot -Tsvg ../answers/graphiz_output.dot -o ../answers/out.svg
+rm ../answers/graphiz_output.dot
